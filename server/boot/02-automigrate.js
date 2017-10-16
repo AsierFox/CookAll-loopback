40f724538f71d8fedf49a0fda0af163867d736a5
asierg <asigonlo@gmail.com>
2017-10-16 12:25:54 +0200

    Set pm2, boot scripts and recipe base model

---
+++ server/boot/02-automigrate.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let loopback = require('loopback');
+
+module.exports = function (app) {
+
+  let resetDB = false;
+
+  if (resetDB) {
+
+    // app.dataSources.activiMysql.automigrate(function () {
+      // Create models
+    // };
+  }
+};
---
+++ server/boot/03-print-models.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (app) {
+  var models = [];
+
+  Object.keys(app.models).forEach(function (model) {
+    var modelName = app.models[model].modelName;
+
+    if (models.indexOf(modelName) === -1) {
+      models.push(modelName);
+    }
+  });
+
+  console.log('All models: ', models);
+};
