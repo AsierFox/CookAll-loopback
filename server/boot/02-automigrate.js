43eeea563feb15a339a7c7e598ff039ed438759c
asierg <asigonlo@gmail.com>
2017-10-17 16:24:27 +0200

    Seeding database & upload some photos for recipes

--- server/boot/02-automigrate.js
+++ server/boot/02-automigrate.js
@@ -1,11 +1,17 @@
 'use strict';
 
 let loopback = require('loopback');
+let asyncSeries = require('async/series');
 
 module.exports = function (app) {
 
   let resetDB = true;
   let seedDB = true;
+
+  let Profile = app.models.Profile;
+  let Recipe = app.models.Recipe;
+  let RecipeCategory = app.models.RecipeCategory;
+  let RecipeMainPhoto = app.models.RecipeMainPhoto;
 
   if (resetDB) {
 
@@ -13,6 +19,71 @@ module.exports = function (app) {
 
       if (seedDB) {
         // Create models
+        asyncSeries([
+          function (cb) {
+            Profile.create({ // User id, 1
+              email: 'a@a.a',
+              password: 'a',
+              username: 'Asier',
+              name: 'Asier',
+              surnames: 'González López',
+              gender: 'm'
+            }, cb);
+          },
+          function (cb) {
+            Profile.create({ // User id, 2
+              email: 'b@b.b',
+              password: 'b',
+              username: 'Jasone',
+              name: 'Jasone',
+              surnames: 'Arranz Martinez',
+              gender: 'f'
+            }, cb);
+          },
+          function (cb) {
+            Recipe.create({
+              title: 'Iced Pumpkin Cookies',
+              description: 'Wonderful spicy iced pumpkin cookies that both kids and adults love!',
+              cookingTime: '1 h 20 m',
+              calories: 122,
+              userId: 1
+            }, cb);
+          },
+          function (cb) {
+            Recipe.create({
+              title: 'To Die For Fettuccine Alfredo',
+              description: 'This is a recipe that I created by modifying my mother\'s recipe. My boyfriend is a fettuccine Alfredo connoisseur and he scrapes the pan every time! I get nothing but rave reviews when I make this dish. I must warn you, this recipe is not for the health conscious. Enjoy!',
+              cookingTime: '30 m',
+              calories: 964,
+              userId: 2
+            }, cb);
+          },
+          function (cb) {
+            RecipeCategory.create({
+              name: 'Sweet desserts',
+              recipeId: 1
+            }, cb);
+          },
+          function (cb) {
+            RecipeCategory.create({
+              name: 'Italian',
+              recipeId: 2
+            }, cb);
+          },
+          function (cb) {
+            RecipeMainPhoto.create({
+              recipeId: 1
+            }, cb);
+          },
+          function (cb) {
+            RecipeMainPhoto.create({
+              recipeId: 2
+            }, cb);
+          }
+        ],
+        function () {
+          console.log('All models seeded!');
+        });
       }
     });
 
